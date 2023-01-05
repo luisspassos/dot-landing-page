fbed33cbb93e697124e81154ca4856ced1c27e43
Luís Passos <luis.passos013@gmail.com>
2023-01-04 21:11:09 -0300

    add styles

--- interactiveCircle.js
+++ interactiveCircle.js
@@ -1,6 +1,52 @@
-const yellowButton = document.querySelector('.interactive .circle .yellow')
+const circle = document.querySelector('.interactive .circle')
 
-console.log(yellowButton)
+const buttons = document.querySelectorAll('.interactive button')
+const [buttonYellow, buttonRed, buttonGray] = buttons
 
-yellowButton.addEventListener('click', () => { alert('ias') })
+const texts = document.querySelectorAll('.interactive .circle strong')
+const [textYellow, textRed, textGray] = texts
 
+function handleColorChange(button, color, text) {
+
+  function resetStyles() {
+    circle.className = 'circle'
+
+    for (const text of texts) {
+      text.classList.remove('active')
+    }
+
+    for (const button of buttons) {
+      button.classList.remove('active')
+
+      const icon = button.firstElementChild
+
+      icon.src = 'icones/icon-plus.svg'
+    }
+  }
+
+  resetStyles()
+
+  text.classList.add('active')
+  circle.classList.add(color)
+
+  button.classList.add('active')
+
+  const icon = button.firstElementChild;
+
+  icon.src = 'icones/icon-minus.svg'
+
+
+}
+
+
+buttonYellow.addEventListener('click', () => {
+  handleColorChange(buttonYellow, undefined, textYellow)
+})
+
+buttonRed.addEventListener('click', () => {
+  handleColorChange(buttonRed, 'red', textRed)
+})
+
+buttonGray.addEventListener('click', () => {
+  handleColorChange(buttonGray, 'gray', textGray)
+})
